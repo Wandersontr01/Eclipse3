df45952611cb8e511a6f1dcfa9f7b2940ed3f99a
Wandersontr01 <wanderson.franca@aluno.faesa.br>
2022-11-12 13:22:41 -0300

    Main
    Addmain

---
+++ src/Escola/Main.java
@@ -0,0 +1,12 @@
+package Escola;
+import java.util.*;
+
+public class Main {
+	static Scanner entrada = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("MENU");
+		
+	}
+
+}
